5ded6a02005ec8b8bfd0e077cea777d6bfcf3c35
preshgit <preciousdesk10@gmail.com>
2025-03-13 17:56:28 +0100

    enhancement: removed redundant code in button component

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -33,36 +33,3 @@ const Button = ({
 };
 
 export default Button;
-
-// const Button = ({
-//   text,
-//   link,
-//   type,
-//   form,
-//   className,
-// }: {
-//   text: string;
-//   link: string;
-//   type: string;
-//   form?: boolean;
-//   className?: string;
-// }) => {
-//   return (
-//     <a
-//       href={link}
-//       className={`${
-//         type === "primary"
-//           ? "bg-customPink hover:bg-customPink/50 hover:border-transparent hover:text-white text-white text-center border-2"
-//           : "bg-black text-customPink border-2 text-center border-customPink hover:border-customPink/50 hover:text-white"
-//       } ${
-//         form ? "w-full" : ""
-//       } border-customPink rounded-lg text-[12px] md:text-[16px] font-bold duration-200 transition-all p-2 md:py-2.5 md:px-4 ${
-//         className || ""
-//       }`}
-//     >
-//       {text}
-//     </a>
-//   );
-// };
-
-// export default Button;
