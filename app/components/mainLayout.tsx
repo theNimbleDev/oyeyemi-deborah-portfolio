b5f07077fa6e088d2d1259ca8666063b8a20ab39
preshgit <preciousdesk10@gmail.com>
2025-09-04 18:30:17 +0100

    Feature: md screen stling

--- app/components/mainLayout.tsx
+++ app/components/mainLayout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "@/app/components/navbar";
-import { Footer } from "@/app/components/footer";
+import Navbar from '@/app/components/navbar';
+import { Footer } from '@/app/components/footer';
 
 export default function MainLayout({
   children,
@@ -8,11 +8,11 @@ export default function MainLayout({
 }) {
   return (
     <>
-      <div className="bg-background text-foreground min-h-screen flex flex-col mx-auto">
-        <header className=" sticky top-0 z-50">
+      <div className='bg-background text-foreground min-h-screen flex flex-col mx-auto md:px-8'>
+        <header className=' sticky top-0 z-50'>
           <Navbar />
         </header>
-        <main className="px-4 md:px-0 md:max-w-[1128px] container mx-auto">
+        <main className='px-4 md:px-0 md:max-w-[1128px] container mx-auto'>
           {children}
         </main>
       </div>
